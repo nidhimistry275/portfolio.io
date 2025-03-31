6627c517bcec87da69ccdd3ed0a8ebedb6b609e2
nidhimistry275 <mistrynidhi94@gmail.com>
2025-03-30 23:05:30 -0700

    Base

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import SkillCard from "./components/SkillCard";
 import AnimatedCursor from "react-animated-cursor";
 import { db } from "./firebase";
 import { doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
+import firebase from "./firebase";
 
 const trackVisitors = async () => {
   const docRef = doc(db, "siteStats", "views");
