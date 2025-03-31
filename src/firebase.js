f6e97ee174323574d1be70c24ba8bb18ead9a745
nidhimistry275 <mistrynidhi94@gmail.com>
2025-03-30 23:08:38 -0700

    Firebase added

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
+import firebase from "./firebase";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import firebase from "./firebase";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
